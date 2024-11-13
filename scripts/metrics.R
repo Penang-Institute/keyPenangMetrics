dd9e391cf491dc0f0fd0a44c51574b814f865ef3
Philip Khor <35039795+philip-khor@users.noreply.github.com>
2024-11-13 11:24:33 +0800

    output yamls for pistats

--- scripts/metrics.R
+++ scripts/metrics.R
@@ -47,7 +47,7 @@ req_ur <- base_request |>
 list(
   `GDP growth` = req_gdp_growth, 
   `Median gross household income` = req_income, 
-  `Year-on-year CPI inflation` = req_cpi, 
+  `CPI inflation, year-on-year` = req_cpi, 
   `Unemployment rate` = req_ur
 ) |> 
   purrr::map(req_perform) |> 
@@ -92,6 +92,53 @@ arrow::read_parquet(
     ), 
     dataset = ifelse(name == "population", "Population", dataset)
   ) |> 
-  select(dataset, value, date_format) |> 
-  write.table("output/metrics.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
+  select(dataset, value, date_format) -> ds
+
+ds |> write.table("output/metrics.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
   
+path <- c(
+  `Population` = "dashboards/pop.html", 
+  `GDP growth` = 'dashboards/gdp.html', 
+  `Median gross household income` = 'dashboards/hhinc.html', 
+  `CPI inflation, year-on-year` = 'dashboards/cpi.html', 
+  `Unemployment rate` = 'dashboards/labour.html'
+)
+ds |> 
+  rename(description = dataset, subtitle = value, title = date_format) |> 
+  mutate(path = path[description]) |> 
+  yaml::write_yaml("output/metrics_grid.yaml", column.major = FALSE)
+
+# update Penang Monthly Statistics RSS
+
+library(xml2)
+
+tmp <- tempfile()
+download.file("https://www.penangmonthly.com/tag/statistics/rss", tmp)
+
+content <- readLines(tmp)
+unlink(tmp)
+read_xml(content) |> 
+  xml_find_all("./channel/item") |> 
+  as_list() -> res
+
+title <- sapply(res, \(x) x$title[[1]])
+# date <- sapply(res, \(x) x$pubDate[[1]]) |> 
+#     substr(start = 1, stop = 16) |>
+#     as.Date(format = "%a, %d %b %Y")
+
+date <- sapply(res, \(x) x[names(x) == "category"] |> 
+                 unlist() |> 
+                 grep(pattern = "[[:alpha:]]+\\s[[:digit:]]+", 
+                      value = TRUE) |> unname()) 
+date <- paste("1", date) |> as.Date(format = "%d %B %Y")
+
+
+path <- sapply(res, \(x) x$link[[1]])
+image <- sapply(res, \(x) attributes(x$content)$url)
+author <- sapply(res, \(x) x$creator[[1]])
+
+data.frame(
+  title = title, 
+  date = format(date, "%Y-%m-01"), 
+  path = path, image = image, author = author
+) |> yaml::write_yaml("output/penang-monthly-stats.yaml", column.major = FALSE)
