445c370d742e35d2dba2b9ad6a68ab522304e11b
Philip Khor <35039795+philip-khor@users.noreply.github.com>
2024-11-14 14:46:06 +0800

    move dependencies install to separate r script

---
+++ .github/workflows/install.R
@@ -0,0 +1,7 @@
+install.packages(c("R6", "RColorBrewer", "arrow", "askpass", "assertthat", "bit", 
+"bit64", "cli", "colorspace", "cpp11", "curl", "dplyr", "fansi", 
+"farver", "generics", "glue", "httr2", "labeling", "lifecycle", 
+"lubridate", "magrittr", "munsell", "openssl", "pillar", "pkgconfig", 
+"purrr", "rappdirs", "renv", "rlang", "scales", "stringi", "stringr", 
+"sys", "tibble", "tidyr", "tidyselect", "timechange", "utf8", "jsonlite", 'yaml', 'xml2',
+"vctrs", "viridisLite", "withr"))
